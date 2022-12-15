81afac1fd47047ed446547d1ca215eb8266ddccc
Laxmi kumari <119617244+laxmi2541@users.noreply.github.com>
2022-12-15 04:38:17 -0800

    Add files via upload

---
+++ prime number in c.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int n,i,prime=1;
+printf ("Enter a number");
+scanf("%d",&n);
+for(i=2; i<=n/2; i++)
+{
+if(n%i==0)
+{
+prime=0;
+goto down;
+}
+}
+down:
+	if(prime==1)
+	{
+		printf("%d is a prime number",n);
+}
+else
+{
+	printf("%d is not a prime number",n);
+}
+
+return 0;
+	}	
+
